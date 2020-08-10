b7eb6796ecd5a43ab0413987b64bd3c4262a28a0
Ng-Luu-VT <nguyenluuviettoan@gmail.com>
2020-08-10 19:28:18 +0700

    Initial commit

---
+++ app/src/main/java/com/example/demoretrofit2/activity/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.demoretrofit2.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.example.demoretrofit2.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        binEvent();
+        init();
+    }
+
+    private void init() {
+        TextView tvSignUp = findViewById(R.id.actMain_tvSignUp);
+        tvSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void binEvent() {
+
+    }
+}
